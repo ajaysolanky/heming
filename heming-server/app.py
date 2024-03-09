cbb8b041b33dadc45f3caeef021c68c366b42405
Ajay Solanky <ajsolanky@gmail.com>
2024-03-09 15:25:55 -0500

    Included comments -- there's proably still some minor issues, but this feels like it's far enough to prove out some basic concepts. Claude seems to do a pretty good job. In the future (A) I'll need to think about how to implement all this logic as 'pointers' rather than regenerating the text (in order to ensure stability) (B) I'll need to figure out how to start working in context for longer documents and (C) I'll need to start trying to implement this as a google docs extension

--- heming-server/app.py
+++ heming-server/app.py
@@ -6,13 +6,20 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+response_cache = {}
+
 @app.route('/review', methods=['POST'])
 def review_text():
     data = request.json
     passage = data['passage']
     instruction = data['instruction']
     model = data['model']
-    return jsonify(get_passage_edits(instruction, passage, model))
+    key = (passage, instruction, model)
+    print("SERVER REQUEST:\n", key)
+    if key not in response_cache:
+        response_cache[key] = get_passage_edits(instruction, passage, model)
+    print("SERVER RESPONSE:\n", response_cache[key])
+    return jsonify(response_cache[key])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- heming-server/review_functions.py
+++ heming-server/review_functions.py
@@ -7,10 +7,13 @@ Can you tell me what changes you would make to this passage in order to modify t
 instruction: {instruction}
 passage: {passage}
 
-Make concrete recommendations via inserts and deletes.
+Make concrete recommendations via inserts, deletes, and comments.
 So return the original text but:
 If you want to delete a portion, surround it by the tags "<delete>" and "</delete>"
 If you want to insert some text, surround it by the tags "<insert>" and "</insert>"
+If you want to leave a comment around a portion of text, surround the targeted portion by the tags <comment_n> and </comment_n> , where n is an integer.
+At the bottom of the document, leave a line that looks like: [n]: ~the text of the comment you want to leave~
+Comments should be higher level feedback about the structure and flow of the writing that cannot be communicated via direct modifications.
 
 I will use a compiler to compile this down to text that shows the modifications, sort of how you can track  modifications in google docs
 
@@ -19,12 +22,16 @@ For example, if I provided you with this:
 instruction: rewrite this passage with much more flowery language
 passage: Janice looked nice. In her left hand, she held a rose.
 
-Response:
-Janice looked <delete>nice</delete><insert>stunning beyond compare</insert>. In her left hand, she <delete>held a rose</delete><insert>gripped a scarlet, scintillating rose</insert>.
+RESPONSE:
+<comment_1>Janice looked <delete>nice</delete></comment_1><insert>stunning beyond compare</insert>. <comment_2>In her left hand</comment_2>, she <delete>held a rose</delete><insert>gripped a scarlet, scintillating rose</insert>.
+[1]: Remember that you should be looking to show, not tell
+[2]: Maybe add some description of the hand
 
 Make sure that the resulting passage after suggested modifications is properly written and well constructed.
+Only leave comments if you think they will be particularly helpful. It is perfectly acceptable to leave 0 comments.
+Only return the text of the response, no extra commentary.
 
-Response:"""
+RESPONSE:"""
 
 def get_passage_edits(instruction, passage, model):
     llm_prompt = REVIEW_PROMPT_TEMPLATE.format(
